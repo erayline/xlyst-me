af19e263744f6b6d0c57f6db2217f2baa3b77276
eray sona <eraysonaonline@gmail.com>
2024-07-27 01:10:49 +0300

    login route ekledim user dondurcek

---
+++ src/app/api/login/route.js
@@ -0,0 +1,20 @@
+import { User } from "@/models/User";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+
+export async function POST(request){
+    const email = request.email;
+    const password = request.password;
+
+    if(!email || !password) NextResponse.json({success:false})
+    
+    await mongoose.connect(process.env.MONGO_URI);
+    const user = await User.find({email:email, password:password})
+
+    if(!user){
+        NextResponse.json({success:false})
+    } 
+
+    NextResponse.json({success:true})
+}
