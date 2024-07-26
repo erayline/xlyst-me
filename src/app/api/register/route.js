a22d2e27b2f728e52fbe8e9eeca6980bffb2b79a
eray sona <eraysonaonline@gmail.com>
2024-07-27 00:28:24 +0300

    a

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -6,12 +6,21 @@ import { User } from "@/models/User";
 export async function POST(request){
     const data = await request.json();
     await mongoose.connect(process.env.MONGO_URI);
-    await User.create(
-        {
-        email:data.email,
-        password:data.password,
-        username:data.username
-        }
-    )
-    return NextResponse.json(data)
+    
+    const existingUser = await User.findOne({email:data.email})
+    
+    if (existingUser) {
+        return NextResponse.json(
+            { message: "User already exists" },
+            { status: 409 } // 409 Conflict
+        );
+    }
+
+    await User.create({
+        email: data.email,
+        password: data.password,
+        username: data.username
+    });
+
+    return NextResponse.json(data, { status: 201 });
 }
