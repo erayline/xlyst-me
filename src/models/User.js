f3661bc06b4bf56553290e7fb79f983f3ab13e9c
eray sona <eraysonaonline@gmail.com>
2024-07-26 18:51:43 +0300

    model eklendi

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,10 @@
 import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName:{type:String,default:"user"},
+    email:{type:String},
+    password:{type:String},
+    role:{type:String,default:"user"},
+});
+
+export const User = mongoose.models?.User || mongoose.model("User",userSchema);
