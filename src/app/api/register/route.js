9045f745efb6f6f4825506bdf00297f5a6127907
eray sona <eraysonaonline@gmail.com>
2024-07-26 20:34:14 +0300

    conosele giris

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -4,10 +4,18 @@ import { User } from "@/models/User";
 
 
 export async function POST(request){
-
-    mongoose.connect(process.env.MONGO_URI);
-    User.create({email:"deneme@deneme.deneme",password:"deneme",username:"deneme"})
     const data = await request.json();
+    console.log("*************************")
+    mongoose.connect(process.env.MONGO_URI);
+    console.log("mongoose connected");
+    User.create(
+        {
+        email:"deneme@deneme.deneme",
+        password:"deneme",
+        username:"deneme"
+        }
+    )
+    console.log("new user created");
 
     return NextResponse.json(data)
 }
