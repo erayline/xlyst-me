202b5a82aec0db11b9c6bca51bf1f3d2e4bb64c1
eray sona <eraysonaonline@gmail.com>
2024-07-26 19:53:56 +0300

    ilk register api route

--- src/action/user.js
+++ src/action/user.js
@@ -4,7 +4,16 @@ const register = async (formData)=>{
     const email = formData.get("email")
     const password = formData.get("password")
     const username = formData.get("username")
-    console.log(email);
+
+    let result = await fetch("http://localhost:3000/api/register",{
+        method:"POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify({mesaj:"naber"})
+    })
+    result = await result.json();
+    console.log(email , result);
 }
 
 export {register};
