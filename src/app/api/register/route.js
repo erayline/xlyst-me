f003b9c6b1273ee1bc8a670bcdb2ff82c0a56c03
eray sona <eraysonaonline@gmail.com>
2024-07-26 20:22:22 +0300

    url degisikligi

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
+import { User } from "@/models/User";
 
 
 export async function POST(request){
 
     mongoose.connect(process.env.MONGO_URI);
-    
+    User.create({email:"deneme@deneme.deneme",password:"deneme",username:"deneme"})
     const data = await request.json();
 
     return NextResponse.json(data)
