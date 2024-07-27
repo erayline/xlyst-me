e5ceb88428d497dde5bb5dbcdf1f00d47f4426a7
eray sona <eraysonaonline@gmail.com>
2024-07-27 12:33:54 +0300

    kullanici page eklendi

--- src/app/x/[username]/page.jsx
+++ src/app/x/[username]/page.jsx
@@ -2,21 +2,27 @@ import LinkTile from '@/components/LinkTile'
 import React from 'react'
 
 const Page = ({params}) => {
-    const title= "x";
-    const url="https://www.x.com/_erayl";
-    const icon="https://cdn.prod.website-files.com/5d66bdc65e51a0d114d15891/64cebc6c19c2fe31de94c78e_X-vector-logo-download.png"
-
+    const title = "x";
+    const url = "https://www.x.com/_erayl";
+    const icon = "https://cdn.prod.website-files.com/5d66bdc65e51a0d114d15891/64cebc6c19c2fe31de94c78e_X-vector-logo-download.png"
+    
     return (
-    <div className='m-2 border-2'>
-        <div className='border-2'>
-            <ul>
-                <LinkTile title={title} url={url} icon={icon}/>
-                <LinkTile title={title} url={url} icon={icon}/>
-                {/* {params.username} */}
-            </ul>
+        <div className='m-2'>
+                <ul className='flex flex-col items-center'>
+                    <li className="mt-10">
+                        <h1 className='m-4 text-3xl font-bold'>
+                            <span className='m-1 text-2xl font-extrabold'>$</span>{params.username}
+                        </h1>
+                    </li>
+                    <li className='w-full flex justify-center'>
+                        <ul className='flex flex-col items-start'>
+                            <LinkTile title={title} url={url} icon={icon}/>
+                            <LinkTile title={"instagram"} url={url} icon={"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"}/>
+                        </ul>
+                    </li>
+                </ul>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Page
