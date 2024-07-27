74d6040b866ab030c534fe4042b4a1f5a5deb3fb
eray sona <eraysonaonline@gmail.com>
2024-07-27 17:14:36 +0300

    bıktım

--- src/app/api/link/addLink/route.js
+++ src/app/api/link/addLink/route.js
@@ -2,8 +2,6 @@ import { UserLink } from "@/models/UserLink";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 
-
-
 export async function POST(request) {
     const data = await request.json();
     const { userId, url, title } = data;
@@ -11,13 +9,23 @@ export async function POST(request) {
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
     try {
+        // Check if the userId is a valid ObjectId
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            throw new Error("Invalid userId format");
+        }
 
-        const createdLink = await UserLink.create({ url, title, user: new mongoose.Types.ObjectId(userId) });
-
+        const createdLink = await UserLink.create({
+            url,
+            title,
+            user: new mongoose.Types.ObjectId(userId),
+        });
 
         return NextResponse.json({ success: true, link: createdLink });
     } catch (error) {
         console.error("Error creating link:", error);
         return NextResponse.json({ success: false, error: error.message });
+    } finally {
+        // Disconnect from the database
+        await mongoose.disconnect();
     }
 }
--- src/models/UserLink.js
+++ src/models/UserLink.js
@@ -1,12 +1,11 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const userLinkSchema = mongoose.Schema({
-    user:{type:mongoose.SchemaTypes.ObjectId},
-    title:{type:String, default:"Link"},
-    url:{type:String, default:"https://x.com/_erayl"},
-    
-    icon:{type:String,default:"https://cdn.prod.website-files.com/5d66bdc65e51a0d114d15891/64cebc6c19c2fe31de94c78e_X-vector-logo-download.png"},
-    click:{type:Number,default:0}
-})
+const userLinkSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+    title: { type: String, default: "Link" },
+    url: { type: String, default: "https://x.com/_erayl" },
+    icon: { type: String, default: "https://cdn.prod.website-files.com/5d66bdc65e51a0d114d15891/64cebc6c19c2fe31de94c78e_X-vector-logo-download.png" },
+    click: { type: Number, default: 0 }
+});
 
-export const UserLink = mongoose.models?.UserLink || mongoose.model("UserLink",userLinkSchema);
+export const UserLink = mongoose.models?.UserLink || mongoose.model("UserLink", userLinkSchema);
