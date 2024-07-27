8912058615934abd1ed87b0f9d45931e88f6a419
eray sona <eraysonaonline@gmail.com>
2024-07-27 14:59:30 +0300

    a

--- src/action/link.js
+++ src/action/link.js
@@ -3,7 +3,7 @@ const addLink = async (formData)=>{
     const title = formData.get('title');
     const url = formData.get('url');
 
-    console.log(userId);
+    console.log(url);
     // buraya endpoint gelecek.
 
 }
