77456a8f911276f05736d92b0806d0a105024bd6
eray sona <eraysonaonline@gmail.com>
2024-07-27 17:04:46 +0300

    link ekleme bitiyor

--- src/app/api/link/addLink/route.js
+++ src/app/api/link/addLink/route.js
@@ -12,8 +12,8 @@ export async function POST(request) {
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
     try {
-        
-        const createdLink = await UserLink.create({ url, title, user: userId });
+
+        const createdLink = await UserLink.create({ url, title, user: mongoose.Types.ObjectId(userId); });
 
 
         return NextResponse.json({ success: true, link: createdLink });
