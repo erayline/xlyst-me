14cbf3a1ee6fc83cd45baa154bb10c764ae392b6
eray sona <eraysonaonline@gmail.com>
2024-07-27 14:55:49 +0300

    a

--- src/action/link.js
+++ src/action/link.js
@@ -1,9 +1,7 @@
-import { auth } from "@/auth";
 
 const addLink = async (formData)=>{
     const title = formData.get('title');
     const url = formData.get('url');
-    const userId = await auth();
 
     console.log(userId);
     // buraya endpoint gelecek.
