5836421a618bfba9ee50463a795ce723b764836b
eray sona <eraysonaonline@gmail.com>
2024-07-27 20:48:20 +0300

    A

--- src/app/api/link/getUserLinks/route.js
+++ src/app/api/link/getUserLinks/route.js
@@ -1,26 +1,23 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import { UserLink } from "@/models/UserLink";
 import { NextResponse } from "next/server";
 import { User } from "@/models/User";
 
-export async function POST(request) {
+
+export async function POST(request){
     const data = await request.json();
-    const { username } = data;
+    const {username} = data;
 
-    await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
 
-    const user = await User.findOne({ userName: username });
+    await mongoose.connect(process.env.MONGO_URI);
+    const user = await User.findOne({userName:username});
     console.log(user);
 
-    if (!user) return NextResponse.json({ success: false });
+    if(!user) return NextResponse.json({success:false})
+    let userId = user.id;
+    
+    const linkList = await UserLink.find({user:userId})
+    console.log(linkList)
+    return NextResponse.json({success:true,liste:linkList})
 
-    const userId = user._id;
-
-    const linkList = await UserLink.find({ user: userId });
-    console.log(linkList);
-
-    return NextResponse.json({ success: true, liste: linkList });
 }
