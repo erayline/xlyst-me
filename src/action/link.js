0555b4c2493316ada3adbe9fb3f0576136b903a6
eray sona <eraysonaonline@gmail.com>
2024-07-27 15:08:26 +0300

    a

--- src/action/link.js
+++ src/action/link.js
@@ -1,9 +1,19 @@
+import { auth } from "@/auth";
 
 const addLink = async (formData)=>{
     const title = formData.get('title');
     const url = formData.get('url');
 
-    console.log(url);
+    const session = await auth();
+    console.log(session)
+    // const response = await fetch("https://platinleaf.vercel.app/api/link/addLink",{
+    //     method:"POST",
+    //     body:{
+    //         title:title,
+    //         url:url,
+    //     }
+    // })
+    
     // buraya endpoint gelecek.
 
 }
