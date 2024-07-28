3be5750e0e5c44fb98c11cdbb221baa64e1dfd4f
eray sona <eraysonaonline@gmail.com>
2024-07-28 14:33:50 +0300

    a

--- src/app/x/[username]/page.jsx
+++ src/app/x/[username]/page.jsx
@@ -7,8 +7,6 @@ import { useState,useEffect } from 'react';
 
 const Page = ({params}) => {
     const [userList,setUserList] = useState(null);
-    const [session,setSession] = useState(null);
-    const { data, status } = useSession();
 
     useEffect(async ()=>{
         const result = await fetch('https://platinleaf.vercel.app/api/link/getUserLinks', {
@@ -25,7 +23,6 @@ const Page = ({params}) => {
             return <LinkTile key={index} title={element.title} url={element.url} icon={element.icon}/>
         })
         setUserList(res);
-        setSession(data);
     },[])
     
     // const linkListesiJsx = datam.map((element,index) => {
@@ -36,8 +33,6 @@ const Page = ({params}) => {
         <div className='m-2'>
                 <ul className='flex flex-col items-center'>
                     <li className="mt-10">
-                        {session.user.username == params.username && <a className='m-4 p-4 bg-black text-white' href="https://platinleaf.vercel.app/addLink">link ekle</a>}
-                        
                         <h1 className='m-4 text-3xl font-bold'>
                             <span className='m-1 text-2xl font-extrabold'>$</span>{params.username}
                         </h1>
