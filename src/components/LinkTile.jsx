99686e7b9cc9fc09938b730e67a1395a0e16babb
eray sona <eraysonaonline@gmail.com>
2024-07-28 23:03:45 +0300

    a

--- src/components/LinkTile.jsx
+++ src/components/LinkTile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const LinkTile = ({title,url,icon,onDelete}) => {
+const LinkTile = ({title,url,icon}) => {
   return (
     <li className='m-2 flex justify-center border-l-4  border-black group'>
         <a target='_blank' href={url} className='flex flex-row justify-center gap-4 m-1 p-4 items-center duration-200 rounded-r-md group-hover:bg-black'>
