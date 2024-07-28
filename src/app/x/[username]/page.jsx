d4a4a8c2b234de8f88470e37a8c3e04ad966c1cd
eray sona <eraysonaonline@gmail.com>
2024-07-28 15:27:11 +0300

    a

--- src/app/x/[username]/page.jsx
+++ src/app/x/[username]/page.jsx
@@ -7,6 +7,7 @@ import { useState,useEffect } from 'react';
 
 const Page = ({params}) => {
     const [userList,setUserList] = useState(null);
+    const { data:session, status } = useSession();
 
     useEffect(async ()=>{
         const result = await fetch('https://platinleaf.vercel.app/api/link/getUserLinks', {
@@ -24,6 +25,12 @@ const Page = ({params}) => {
         })
         setUserList(res);
     },[])
+
+    if(status==="loading"){
+        return (<div>
+            havalı bir yukleme animasyonu
+        </div>)
+    }   
     
     // const linkListesiJsx = datam.map((element,index) => {
     //     <LinkTile key={index} title={element.title} url={element.url} icon={element.icon}/>
@@ -32,6 +39,7 @@ const Page = ({params}) => {
     return (
         <div className='m-2'>
                 <ul className='flex flex-col items-center'>
+                    {session.user.username == params.username && <button>oldu çalıştı</button>}
                     <li className="mt-10">
                         <h1 className='m-4 text-3xl font-bold'>
                             <span className='m-1 text-2xl font-extrabold'>$</span>{params.username}
