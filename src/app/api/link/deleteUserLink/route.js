49629cc80f2ae55ea9b41ea215f71bb3ba29c983
eray sona <eraysonaonline@gmail.com>
2024-07-29 21:20:25 +0300

    a

--- src/app/api/link/deleteUserLink/route.js
+++ src/app/api/link/deleteUserLink/route.js
@@ -1,7 +1,8 @@
 import { UserLink } from '@/models/UserLink';
 import mongoose from 'mongoose';
 
-export async function POST(req,res) {
+export async function POST(req, res) {
+    try {
         await mongoose.connect(process.env.MONGO_URI);
 
         const { id } = JSON.parse(req.body);
@@ -9,4 +10,7 @@ export async function POST(req,res) {
         await UserLink.findByIdAndDelete(id);
 
         res.status(200).json({ message: 'Link deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
