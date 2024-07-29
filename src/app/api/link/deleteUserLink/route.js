dc0052f27aeeeb1c038c0c3902c8f1016c915027
eray sona <eraysonaonline@gmail.com>
2024-07-29 21:14:00 +0300

    delete req

--- src/app/api/link/deleteUserLink/route.js
+++ src/app/api/link/deleteUserLink/route.js
@@ -2,6 +2,10 @@ import { UserLink } from '@/models/UserLink';
 import mongoose from 'mongoose';
 
 export default async function handler(req, res) {
+    if (req.method !== 'DELETE') {
+        return res.status(405).json({ message: 'Method Not Allowed' });
+    }
+
     try {
         await mongoose.connect(process.env.MONGO_URI);
 
