97ef10dfd4b1a07f60fd321b93f9a540a584e15a
eray sona <eraysonaonline@gmail.com>
2024-07-29 19:43:22 +0300

    🎧

--- src/app/x/[username]/page.jsx
+++ src/app/x/[username]/page.jsx
@@ -3,58 +3,59 @@
 import LinkTile from '@/components/LinkTile'
 import { useSession } from 'next-auth/react';
 import React from 'react'
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-const Page = ({params}) => {
-    const [userList,setUserList] = useState(null);
-    const { data:session, status } = useSession();
+const Page = ({ params }) => {
+    const [userList, setUserList] = useState(null);
+    const { data: session, status } = useSession();
 
-    useEffect(async ()=>{
-        const result = await fetch('https://platinleaf.vercel.app/api/link/getUserLinks', {
-            method:"POST",
-            body: JSON.stringify({
-                username: params.username
-            })
-        }
-        ) // Replace with your API endpoint
-        let res = await result.json();
-        console.log(res);
-        //link tile oluşturuyoruz
-        res = res.liste.map((element,index)=> {
-            return <LinkTile key={index} title={element.title} url={element.url} icon={element.icon}/>
-        })
-        setUserList(res);
-    },[])
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch('https://platinleaf.vercel.app/api/link/getUserLinks', {
+                method: "POST",
+                body: JSON.stringify({
+                    username: params.username
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            let res = await result.json();
+            console.log(res);
 
-    if(status==="loading" || userList == null){
-        return (<div>
-            🍸
-        </div>)
-    }   
-    
-    // const linkListesiJsx = datam.map((element,index) => {
-    //     <LinkTile key={index} title={element.title} url={element.url} icon={element.icon}/>
-    // }) 
+            // Map the response to LinkTile components
+            res = res.liste.map((element, index) => {
+                return <LinkTile key={index} title={element.title} url={element.url} icon={element.icon} />
+            });
+            setUserList(res);
+        };
 
-    const addLinkJsx = <a className='inline-block px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-black hover:bg-yellow-500 hover:text-black' href='https://platinleaf.vercel.app/addLink'>Add Link</a>
+        fetchData().catch(console.error);
+    }, [params.username]);
+
+    if (status === "loading" || userList == null) {
+        return (<div>🍸</div>);
+    }
+
+    const addLinkJsx = <a className='inline-block px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-black hover:bg-yellow-500 hover:text-black' href='https://platinleaf.vercel.app/addLink'>Add Link</a>;
 
     return (
         <div className='m-2'>
-                <ul className='flex flex-col items-center'>
-                    {(session) && ((session.user.username == params.username) && addLinkJsx)}
-                    <li className="mt-10">
-                        <h1 className='m-4 text-3xl font-bold'>
-                            <span className='m-1 text-2xl font-extrabold'>$</span>{params.username}
-                        </h1>
-                    </li>
-                    <li className='w-full flex justify-center'>
-                        <ul className='flex flex-col items-start'>
-                            {userList}
-                        </ul>
-                    </li>
-                </ul>
+            <ul className='flex flex-col items-center'>
+                {(session) && ((session.user.username == params.username) && addLinkJsx)}
+                <li className="mt-10">
+                    <h1 className='m-4 text-3xl font-bold'>
+                        <span className='m-1 text-2xl font-extrabold'>$</span>{params.username}
+                    </h1>
+                </li>
+                <li className='w-full flex justify-center'>
+                    <ul className='flex flex-col items-start'>
+                        {userList}
+                    </ul>
+                </li>
+            </ul>
         </div>
-    )
+    );
 }
 
-export default Page
+export default Page;
