3c5eb47fe5c97c45abf8b9c2d881396af1a5be4d
eray sona <eraysonaonline@gmail.com>
2024-07-30 16:39:04 +0300

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
 import FirstSection from "@/components/FirstSection";
 import Footer from "@/components/Footer";
-import { useSession } from "next-auth/react";
-export default function Home() {
-  const { data: session, status } = useSession();
 
-  
+export default async function Home() {
+
+  const session = await auth();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
