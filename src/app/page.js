a70450cfb2915b4c367c20f3396be82a8d8c2a1b
eray sona <eraysonaonline@gmail.com>
2024-07-30 15:49:16 +0300

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 import FirstSection from "@/components/FirstSection";
 import Footer from "@/components/Footer";
+import { useSession } from "next-auth/react";
 export default function Home() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <></> 
+
+  let username="";
+  if(session) username = session.user.username;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-    <FirstSection/>
+    <FirstSection loggedIn={session} username={username}/>
     <Footer/>
     </main>
   );
