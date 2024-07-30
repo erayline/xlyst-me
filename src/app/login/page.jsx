07774fdec9413b900211b51febf9d8f322e133e7
eray sona <eraysonaonline@gmail.com>
2024-07-30 16:17:55 +0300

    a

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,33 +1,35 @@
 'use client'
-
-
 import { login } from "@/action/user";
 import { useSession } from "next-auth/react";
-import { redirect } from "next/dist/server/api-utils";
+import { useRouter } from 'next/navigation';
 import Link from "next/link";
+import { useEffect } from 'react';
 
 const Login = () => {
-
     const { data: session, status } = useSession();
+    const router = useRouter();
 
-    if(status === "loading") return <div className='m-10 text-center text-9xl'>🍸</div>
-    console.log(session);
+    useEffect(() => {
+        if (session && session.user?.username) {
+            router.push(`/x/${session.user.username}`);
+        }
+    }, [session, router]);
+
+    if (status === "loading") return <div className='m-10 text-center text-9xl'>🍸</div>
     
-    if(session) redirect('/x/'+session.user?.username);
+    if (session) return null; // Return null while redirecting
 
     return (
-      <div className='w-full flex flex-col my-32 items-center justify-center'>
-        <form action={login}
- className='max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl flex flex-col justify-center items-center border-4 p-4 md:p-10 border-primary rounded'>
-          <h2 className='font-bold text-2xl md:text-3xl underline underline-offset-8 m-4'>Login</h2>
-          <input className='w-full p-2 mb-4 border rounded' placeholder='email' type="email" name="email" id="email" required />
-          <input className='w-full p-2 mb-4 border rounded' placeholder="****" type="password" name="password" id="password" required />
-          <button type='submit' className='bg-black text-white p-2 rounded w-full'>Login</button>
-          <p className='mt-4'>Don't have an account? <Link href="/register" className='font-bold text-black p-2'>Register</Link></p>
-        </form>
-      </div>
+        <div className='w-full flex flex-col my-32 items-center justify-center'>
+            <form action={login} className='max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl flex flex-col justify-center items-center border-4 p-4 md:p-10 border-primary rounded'>
+                <h2 className='font-bold text-2xl md:text-3xl underline underline-offset-8 m-4'>Login</h2>
+                <input className='w-full p-2 mb-4 border rounded' placeholder='email' type="email" name="email" id="email" required />
+                <input className='w-full p-2 mb-4 border rounded' placeholder="****" type="password" name="password" id="password" required />
+                <button type='submit' className='bg-black text-white p-2 rounded w-full'>Login</button>
+                <p className='mt-4'>Don't have an account? <Link href="/register" className='font-bold text-black p-2'>Register</Link></p>
+            </form>
+        </div>
     )
-
-  }
+}
 
 export default Login
