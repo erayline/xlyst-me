57c74cb71c8eb0b232a16f8fefa8e1958cabc237
eray sona <eraysonaonline@gmail.com>
2024-07-30 12:01:09 +0300

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,15 @@ const Footer = () => {
   return (
     <div className='w-11/12 mt-10 h-32 border-t-2 border-zinc-700  flex flex-row justify-around'>
         <div>
-            <h3 className='text-gray-200 underline text-center'>More</h3>
-            <ul className='text-gray-400 gap-4 my-4 text-center flex flex-col'>
-                <li><a href="https://eggable-mobile.vercel.app/">Eggable</a></li>
+            <h3 className='text-zinc-500 underline text-center'>More</h3>
+            <ul className='text-zinc-400 gap-4 my-4 text-center flex flex-col'>
+                <li><a href="https://eggable-mobile.vercel.app/">🐣 Eggable</a></li>
+                <li><a href="https://platinleaf.vercel.app/">🍸 Prestige List</a></li>
             </ul>
         </div>
         <div>
-            <h3 className='text-gray-200 underline  text-center'>Me</h3>
-            <ul className='text-gray-400 gap-4 my-4 text-center flex flex-col'>
+            <h3 className='text-zinc-500 underline  text-center'>Me</h3>
+            <ul className='text-zinc-400 gap-4 my-4 text-center flex flex-col'>
                 <li><a href="https://x.com/_erayl/bio">Portfolio</a></li>
                 <li><a href="https://x.com/_erayl">𝕏 link</a></li>
             </ul>
