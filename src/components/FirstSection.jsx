442d33f6f58bc5397d30173f421242049175e6f6
eray sona <eraysonaonline@gmail.com>
2024-07-30 21:01:04 +0300

    a

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -11,7 +11,7 @@ const FirstSection =async  () => {
       <div className='  page-width max-w-screen-lg section-h justify-between items-center flex flex-col gap-10 lg:flex-row'>
       
         <section className='  h-auto flex flex-col items-center text-center sm:items-start sm:text-left'>
-          <h1 className='  text-zinc-800 my-3 text-3xl lg:text-5xl font-bold'>Simple:<br/><br/> Everything about you, in one place.</h1>
+          <h1 className='  text-zinc-800 my-3 text-3xl lg:text-5xl font-bold'>Simple:<br/> Everything about you, in one place.</h1>
           <h2 className='  my-3 font-medium text-gray-500 w-64 lg:w-3/4'>&nbsp;&nbsp;&nbsp;No need to re enter every link you have in to the bio sections. No needless design, no useless tools. Simple, elegant and usefull. With this, save time.</h2>
           
           <div className='my-3 flex justify-end'>
