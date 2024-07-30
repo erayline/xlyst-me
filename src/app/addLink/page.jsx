dbc019b2623b62a18afcd10eefbd0a3e071c4182
eray sona <eraysonaonline@gmail.com>
2024-07-31 00:37:14 +0300

    navbar

--- src/app/addLink/page.jsx
+++ src/app/addLink/page.jsx
@@ -73,7 +73,7 @@ const Register = () => {
     </button>)
   })
   return (
-    <div className='w-full flex flex-col my-32 items-center justify-center'>
+    <div className='w-full flex flex-col my-16 lg:my-32 items-center justify-center'>
       <form action={addLink} className='max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl flex flex-col justify-center items-center border-4 p-4 md:p-10 border-primary rounded'>
         <h2 className='font-bold text-2xl md:text-3xl underline underline-offset-8 m-4'>Add a Link</h2>
         <input className='w-full p-2 mb-4 border rounded' placeholder='Title(X, my products...)' type="text" name="title" id="title" required />
