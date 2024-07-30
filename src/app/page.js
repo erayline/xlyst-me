cb00060d78dccaf3e393dd9c52aa329efb42d9bc
eray sona <eraysonaonline@gmail.com>
2024-07-30 15:52:00 +0300

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,10 @@
 import FirstSection from "@/components/FirstSection";
 import Footer from "@/components/Footer";
-import { useSession } from "next-auth/react";
 export default function Home() {
-  const { data: session, status } = useSession();
-
-  if (status === "loading") return <></> 
-
-  let username="";
-  if(session) username = session.user.username;
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-    <FirstSection loggedIn={session} username={username}/>
+    <FirstSection/>
     <Footer/>
     </main>
   );
--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import MobileView from './MobileView'
 
-const FirstSection = ({loggedIn,username}) => {
-
-
+const FirstSection = () => {
   return (
     <div className='flex flex-col items-center p-4 sm:p-8'>
       <div className='  page-width max-w-screen-lg section-h justify-between items-center flex flex-col gap-10 lg:flex-row'>
@@ -12,19 +10,12 @@ const FirstSection = ({loggedIn,username}) => {
           <h1 className='  text-zinc-800 my-3 text-3xl lg:text-5xl font-bold'>Simple:<br/><br/> Everything about you, in one place.</h1>
           <h2 className='  my-3 font-medium text-gray-500 w-64 lg:w-3/4'>&nbsp;&nbsp;&nbsp;No need to re enter every link you have in to the bio sections. No needless design, no useless tools. Simple, elegant and usefull. With this, save time.</h2>
           <div className='my-3 flex justify-end'>
-            {loggedIn ? <a
-                  href={"https://platinleaf.vercel.app/x/" + username}
-                  className="p-3 rounded-xl bg-amber-400 text-black font-semibold"
-                >
-                  Profile
-                </a> :
                 <a
                   href="https://platinleaf.vercel.app/login"
                   className="p-3 rounded-xl bg-amber-400 text-black font-semibold"
                 >
                   Register, now
                 </a>
-                }
             </div>
         </section>
         
