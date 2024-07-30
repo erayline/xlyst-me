e899cee49e0e837907a36db01d35a318f607d314
eray sona <eraysonaonline@gmail.com>
2024-07-30 12:30:08 +0300

    landing page

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,6 @@ export default function RootLayout({ children }) {
         <SessionProvider>
           <Navbar/>
           {children}
-          <Footer/>
         </SessionProvider>
       </body>
     </html>
--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -6,13 +6,12 @@ const FirstSection = () => {
       <div className='page-width max-w-screen-lg section-h justify-between items-center flex flex-col lg:flex-row'>
       
         <section className='h-auto flex flex-col items-center text-center sm:items-start sm:text-left'>
-          <h1 className='text-white m-3 text-2xl sm:text-3xl font-bold'>Eggable: merge, relax and listen!</h1>
-          <h2 className='m-3 font-medium text-gray-500 w-64 sm:w-96'>You can merge endlessly. Also you can upgrade, prestige and relax endlessly too</h2>
-          <a href='https://play.google.com/store/apps/details?id=com.folinx.eggable' target='_blank' className='m-3 p-3 w-28 sm:w-32 text-center font-bold bg-yellow-500 rounded-xl hover:rounded-sm hover:bg-white select-none'>PlayStore</a>
+          <h1 className='text-white m-3 text-2xl sm:text-3xl font-bold'>Everything about you, In one place</h1>
+          <h2 className='m-3 font-medium text-gray-500 w-64 sm:w-96'>If you are eligible, you will have this.</h2>
         </section>
         
         <section className='flex flex-col items-center sm:items-start'>
-          <h2 className='text-white text-xl sm:text-2xl m-3'>Some eggs🐣</h2>
+          <h2 className='text-white text-xl sm:text-2xl m-3'>🍸</h2>
           <div className='flex flex-col justify-center sm:justify-start'>
             <p>buraya uygulamanın showcase'i konulacak</p>
           </div>
