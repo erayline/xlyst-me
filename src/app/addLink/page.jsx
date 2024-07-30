9d45c774b356f247607107e79ef41469f553ecc9
eray sona <eraysonaonline@gmail.com>
2024-07-31 00:40:23 +0300

    navbar

--- src/app/addLink/page.jsx
+++ src/app/addLink/page.jsx
@@ -73,12 +73,12 @@ const Register = () => {
     </button>)
   })
   return (
-    <div className='w-full flex flex-col my-16 lg:my-32 items-center justify-center'>
+    <div className='w-full flex flex-col my-32 lg:my-16 items-center justify-center'>
       <form action={addLink} className='max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl flex flex-col justify-center items-center border-4 p-4 md:p-10 border-primary rounded'>
         <h2 className='font-bold text-2xl md:text-3xl underline underline-offset-8 m-4'>Add a Link</h2>
         <input className='w-full p-2 mb-4 border rounded' placeholder='Title(X, my products...)' type="text" name="title" id="title" required />
         <input className='w-full p-2 mb-4 border rounded' placeholder="url" type="text" name="url" id="url" required />
-        <div className="w-56 justify-center flex flex-row flex-wrap h-44 mb-4 overflow-x-hidden bg-black rounded-xl overflow-scroll">
+        <div className="w-56 justify-center flex flex-row flex-wrap h-56 lg:h-48 mb-4 overflow-x-hidden bg-black rounded-xl overflow-scroll">
           {chosenIcon}
         </div>
         <input type="hidden" name="iconUrl" id="iconUrl" value={iconUrl}/>
