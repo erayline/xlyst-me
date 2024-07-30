4c93935a7db7c3459eb1cf7d2c1b19c2e3696f3a
eray sona <eraysonaonline@gmail.com>
2024-07-30 16:06:23 +0300

    a

--- src/action/user.js
+++ src/action/user.js
@@ -1,45 +1,52 @@
 "use server"
 
-import { signIn } from "@/auth"
-import { redirect } from "next/navigation"
+import { signIn } from "@/auth";
+import { redirect } from "next/navigation";
 
-const login = async (formData) =>{
-    const email = formData.get('email');
-    const password = formData.get('password');
+const login = async (formData) => {
+  const email = formData.get('email');
+  const password = formData.get('password');
 
-    //TODO: SİGN İN'İ DÜZELt
+  const session = await signIn('credentials', {
+    redirect: false,
+    callbackUrl: "/",
+    email,
+    password
+  });
 
+  if (session?.ok) {
+    redirect(`/x/${session.user.username}`);
+  } else {
+    // Handle login error (optional)
+    console.error("Login failed");
+  }
+};
 
+const register = async (formData) => {
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const username = formData.get("username");
 
-    const session = await signIn('credentials',{
-        redirect:false,
-        callbackUrl:"/",
-        email,
-        password}
-    )
-
-    if (session) redirect('/x');
-}
-
-const register = async (formData)=>{
-    const email = formData.get("email")
-    const password = formData.get("password")
-    const username = formData.get("username")
-
-    let result = await fetch("https://platinleaf.vercel.app/api/register",{
-        method:"POST",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body:JSON.stringify({
-            email:email,
-            password:password,
-            username:username
-        })
+  let result = await fetch("https://platinleaf.vercel.app/api/register", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      password,
+      username
     })
+  });
 
-    result = await result.json();
-    if(result.success) redirect('/login');
-}
+  result = await result.json();
+  
+  if (result.success) {
+    redirect('/login');
+  } else {
+    // Handle registration error (optional)
+    console.error("Registration failed");
+  }
+};
 
-export {register,login};
+export { register, login };
