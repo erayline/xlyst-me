516c9f9db82afc8bce77bb270a94d6c4801eed05
eray sona <eraysonaonline@gmail.com>
2024-07-31 14:13:47 +0300

    model default

--- src/models/UserLink.js
+++ src/models/UserLink.js
@@ -4,7 +4,7 @@ const userLinkSchema = new mongoose.Schema({
     user: { type: String, required:true},
     title: { type: String, default: "Link" },
     url: { type: String, default: "https://x.com/_erayl" },
-    icon: { type: String},
+    icon: { type: String, default:"https://static.thenounproject.com/png/730315-200.png"},
     click: { type: Number, default: 0 }
 });
 
