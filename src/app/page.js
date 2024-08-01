6012c2f1fbd25b49a27f9ac9b148933ce5202213
eray sona <eraysonaonline@gmail.com>
2024-08-01 18:42:06 +0300

    added landing page video

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import FirstSection from "@/components/FirstSection";
 import Footer from "@/components/Footer";
+import SecondSection from "@/components/SecondSection";
 
 export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
     <FirstSection/>
+    <SecondSection/>
     <Footer/>
     </main>
   );
