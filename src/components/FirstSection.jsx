f5fca97ff974afc83ecf5a0ff95d1fdac636e142
eray sona <eraysonaonline@gmail.com>
2024-08-02 21:47:55 +0300

    landing page navbar fix

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -11,7 +11,7 @@ const FirstSection =async  () => {
       <div className='mt-0 lg:mt-10  page-width max-w-screen-lg section-h justify-between items-center flex flex-col gap-10 lg:flex-row'>
 
         <section className='  h-auto flex flex-col items-center text-center sm:items-start sm:text-left'>
-          <h1 className='  text-zinc-800 mb-3 text-3xl lg:text-5xl font-bold'>Simple:<br/> Everything about you,<br/> In one link in bio.</h1>
+          <h1 className='  text-zinc-800 mb-3 text-3xl lg:text-5xl font-bold'>Everything about you,<br/>in this link in bio.</h1>
           <h2 className='  my-3 font-medium text-gray-500 w-64 lg:w-3/4'>&nbsp;&nbsp;&nbsp;Don't enter your links in bio sections over and over again. Gather all your traffics from Instagram, Linkedin, X... in one link in bio page.</h2>
           
           <div className='my-3 flex justify-end'>
