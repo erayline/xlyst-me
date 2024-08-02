55be493ca0089187ccf88af2654eb5a42d96b83b
eray sona <eraysonaonline@gmail.com>
2024-08-02 12:17:32 +0300

    landing page seo

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -9,10 +9,10 @@ const FirstSection =async  () => {
   return (
     <div className='flex flex-col items-center p-4'>
       <div className='mt-0 lg:mt-10  page-width max-w-screen-lg section-h justify-between items-center flex flex-col gap-10 lg:flex-row'>
-      
+
         <section className='  h-auto flex flex-col items-center text-center sm:items-start sm:text-left'>
-          <h1 className='  text-zinc-800 mb-3 text-3xl lg:text-5xl font-bold'>Simple:<br/><br/> Everything about you, in one place.</h1>
-          <h2 className='  my-3 font-medium text-gray-500 w-64 lg:w-3/4'>&nbsp;&nbsp;&nbsp;No need to re enter every link you have in to the bio sections. No needless design, no useless tools. Simple, elegant and usefull. With this, save time.</h2>
+          <h1 className='  text-zinc-800 mb-3 text-3xl lg:text-5xl font-bold'>Simple:<br/> Everything about you,<br/> In one link in bio.</h1>
+          <h2 className='  my-3 font-medium text-gray-500 w-64 lg:w-3/4'>&nbsp;&nbsp;&nbsp;Don't enter your links in bio sections over and over again. Gather all your traffics from Instagram, Linkedin, X... in one link in bio page.</h2>
           
           <div className='my-3 flex justify-end'>
             {session ? <a
@@ -25,7 +25,7 @@ const FirstSection =async  () => {
                   href="https://www.xlyst.me/register"
                   className="p-3 rounded-xl bg-amber-400 text-black font-semibold"
                 >
-                  Register, now
+                  Start for free, forever.
                 </a>
                 }
           </div>
--- src/components/SecondSection.jsx
+++ src/components/SecondSection.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const SecondSection = () => {
 
   return (
-    <div className='flex flex-col items-center p-4'>
-      <div className='mt-0 lg:mt-10  page-width max-w-screen-lg section-h justify-between items-center flex flex-col gap-10 lg:flex-row'>
+    <div className='flex flex-col items-center p-4 my-20 border-y-2 border-zinc-800'>
+      <div className='mt-0 lg:my-10  page-width max-w-screen-lg section-h justify-between items-center flex flex-col gap-10 lg:flex-row'>
       
       <div className='hidden lg:flex'>
             <iframe height={500} width={650} src="https://www.youtube.com/embed/gbngh_Os03k" title="link in bio tool" frameborder="3" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
