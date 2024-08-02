65ab8b2dd820ca286eacc3fe094896232ac2f80d
eray sona <eraysonaonline@gmail.com>
2024-08-02 16:44:07 +0300

    section

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
     <FirstSection/>
     <SecondSection/>
-    <ThirdSection/>
+    {/* <ThirdSection/> */}
     <Footer/>
     </main>
   );
